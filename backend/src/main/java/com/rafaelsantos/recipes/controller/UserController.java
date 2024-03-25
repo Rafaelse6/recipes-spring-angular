ca0696aedf5fdf937b20073c15742e12431b89e6
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 11:45:49 -0300

    Initial users crud

--- backend/src/main/java/com/rafaelsantos/recipes/controller/UserController.java
+++ backend/src/main/java/com/rafaelsantos/recipes/controller/UserController.java
@@ -3,15 +3,27 @@ package com.rafaelsantos.recipes.controller;
 import com.rafaelsantos.recipes.models.User;
 import com.rafaelsantos.recipes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{userId}")
+    public Optional<User> findUserById(@PathVariable Long userId){
+       return userRepository.findById(userId);
+    }
+
 
     @PostMapping("/users")
     public User createUser(@RequestBody User user) throws Exception {
@@ -23,6 +35,16 @@ public class UserController {
 
         return userRepository.save(user);
     }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable Long userId) {
+
+       userRepository.deleteById(userId);
+
+        return "User deleted successfully";
+    }
+
+
 
    /* public User findByEmail(String email) throws Exception{
         User user = userRepository.findByEmail(email);

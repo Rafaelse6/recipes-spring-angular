69a5bb7f1a40f9ac3b8652f9561fbee004f610b7
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 11:43:42 -0300

    Initial db seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_users(password, full_name, email) values ('12345678', 'Test 1', 'test@gmail.com');
+INSERT INTO tb_users(password, full_name, email) values ('12345678', 'Test 2', 'test2@gmail.com');
+INSERT INTO tb_users(password, full_name, email) values ('12345678', 'Test 3', 'test3@gmail.com');

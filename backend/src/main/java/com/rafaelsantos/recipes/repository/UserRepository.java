4f0947ad1891ae5cf06f533168ac507cfdc60a9a
Rafael Santos <rafa.05125@gmail.com>
2024-03-24 23:04:09 -0300

    UserRepository

--- backend/src/main/java/com/rafaelsantos/recipes/repository/UserRepository.java
+++ backend/src/main/java/com/rafaelsantos/recipes/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.rafaelsantos.recipes.repository;public class UserRepository {
+package com.rafaelsantos.recipes.repository;
+
+import com.rafaelsantos.recipes.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String email);
 }

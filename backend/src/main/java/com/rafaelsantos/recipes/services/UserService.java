5c3bf520a6ff99bc73cb9de0c9d268f56bf5ff87
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 16:25:54 -0300

    UserService

--- backend/src/main/java/com/rafaelsantos/recipes/services/UserService.java
+++ backend/src/main/java/com/rafaelsantos/recipes/services/UserService.java
@@ -1,2 +1,11 @@
-package com.rafaelsantos.recipes.services;public class UserService {
+package com.rafaelsantos.recipes.services;
+
+import com.rafaelsantos.recipes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+
 }

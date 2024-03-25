64635856d6ab147248b86d28c48efcc3d8b0bf78
Rafael Santos <rafa.05125@gmail.com>
2024-03-24 23:04:25 -0300

    HomeController, UserController created

--- backend/src/main/java/com/rafaelsantos/recipes/controller/HomeController.java
+++ backend/src/main/java/com/rafaelsantos/recipes/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.rafaelsantos.recipes.controller;public class HomeController {
+package com.rafaelsantos.recipes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String homeController(){
+        return "Welcome to the main page";
+    }
 }
--- backend/src/main/java/com/rafaelsantos/recipes/controller/UserController.java
+++ backend/src/main/java/com/rafaelsantos/recipes/controller/UserController.java
@@ -1,2 +1,34 @@
-package com.rafaelsantos.recipes.controller;public class UserController {
+package com.rafaelsantos.recipes.controller;
+
+import com.rafaelsantos.recipes.models.User;
+import com.rafaelsantos.recipes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) throws Exception {
+
+        User userExists = userRepository.findByEmail(user.getEmail());
+        if(userExists != null){
+            throw new Exception("User exists with this email" + user.getEmail());
+        }
+
+        return userRepository.save(user);
+    }
+
+   /* public User findByEmail(String email) throws Exception{
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new Exception("User not found with email" + email);
+        }
+        return user;
+    }*/
 }

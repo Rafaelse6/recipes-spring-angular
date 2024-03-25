3ea7dc42e146864d5439241da6f23cd4f718a931
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 16:25:46 -0300

    RecipeController

--- backend/src/main/java/com/rafaelsantos/recipes/controller/RecipeController.java
+++ backend/src/main/java/com/rafaelsantos/recipes/controller/RecipeController.java
@@ -1,2 +1,22 @@
-package com.rafaelsantos.recipes.controller;public class RecipeController {
+package com.rafaelsantos.recipes.controller;
+
+import com.rafaelsantos.recipes.models.Recipe;
+import com.rafaelsantos.recipes.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @PostMapping("/api/recipe/user/{userId}")
+    public Recipe createRecipe(@RequestBody Recipe recipe, @PathVariable Long userId){
+
+        return recipeService.createRecipe(recipe, null);
+    }
 }

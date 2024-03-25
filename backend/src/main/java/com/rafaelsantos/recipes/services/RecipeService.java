dc097bf32878317fa1c8305717e41acc50ad315b
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 16:25:34 -0300

    RecipeService, RecipeServiceImpl

--- backend/src/main/java/com/rafaelsantos/recipes/services/RecipeService.java
+++ backend/src/main/java/com/rafaelsantos/recipes/services/RecipeService.java
@@ -1,2 +1,16 @@
-package com.rafaelsantos.recipes.services;public interface RecipeService {
+package com.rafaelsantos.recipes.services;
+
+import com.rafaelsantos.recipes.models.Recipe;
+import com.rafaelsantos.recipes.models.User;
+
+import java.util.List;
+
+public interface RecipeService {
+
+    public List<Recipe> findAllRecipes();
+    public Recipe findRecipeById(Long id) throws Exception;
+    public Recipe createRecipe(Recipe recipe, User user);
+    public Recipe updateRecipe(Recipe recipe, Long id) throws Exception;
+    public void deleteRecipe(Long id) throws Exception;
+    public Recipe likeRecipe(Long recipeId, User user) throws Exception;
 }

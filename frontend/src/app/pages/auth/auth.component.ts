88245456bdad7d2dff4191225eebff9f8764952d
Rafael Santos <rafa.05125@gmail.com>
2024-03-31 22:18:54 -0300

    Auth component created

---
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.scss',
+})
+export class AuthComponent {
+  isRegister = true;
+
+  registrationForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  handleRegister() {
+    console.log('register ', this.registrationForm.value);
+  }
+
+  handleLogin() {
+    console.log('login ', this.loginForm.value);
+  }
+}
